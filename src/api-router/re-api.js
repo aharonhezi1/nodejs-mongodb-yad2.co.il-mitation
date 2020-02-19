c171aed55caab07de900e00144dd323eba1dc507
aharonhezi1 <rahavmatan@gmail.com>
2020-02-19 11:40:24 +0200

    spliting index.js and adding static api for icons

---
+++ src/api-router/re-api.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const router = new express.Router()
+const { addresses, test } = require('../db/israelAddress');
+const RealEstate = require('../db/realEstateModel');
+const moment = require('moment');
+
+
+router.post('/api/real-estate/find-address', (req, res) => {
+	try {
+		let { address } = req.body;
+
+		address = address
+			.trim()
+			.split(' ')
+			.filter(ad => ad !== ''); 
+		let options = [];
+		const BreakException = {};
+		let cities = [],
+			streets = [];
+		for (let i = address.length; i > 0; i--) {
+			for (let j = 0; j + i <= address.length; j++) {
+				options.push(address.slice(j, j + i).join(' '));
+			}
+		}
+		console.log('options: ', options);
+
+		options.forEach((option) => {
+			const length = option.length;
+			try {
+				addresses.forEach((address,i) => {
+					const city = address.city.slice(0, length);
+					const street = address.street.slice(0, length);
+
+					if (street === option) {
+						// || (options[i+1]&&street === (option+" "+options[i+1]))) {
+						streets.push(address);
+					}
+					if (city === option && cities.indexOf(address.city) === -1) {
+						// || (options[i+1]&&city === (option+" "+options[i+1])) )&& cities.indexOf(address.city) === -1) {
+						cities.push(address.city);
+					}
+					if (cities.length + streets.length > 12 && cities.length > 2 && streets.length > 2)
+						throw BreakException;
+				});
+			} catch (e) {
+				if (e !== BreakException) throw e;
+			}
+		});
+		res.send({ cities, streets: streets.slice(0, 7) });
+	} catch (e) {
+		console.log(e);
+
+		res.send(e);
+	}
+});
+router.get('/api/real-estate/:id', async (req, res) => {
+	try {
+		const reType = req.params.id;
+		const queryParams = req.query;
+		console.log(queryParams);
+		let flats;
+		if (!!queryParams) {
+			const { maxRooms, minRooms, minPrice, maxPrice } = queryParams;
+			let { address, propertyType } = queryParams;
+			address = address ? address.split(',').reverse() : '';
+			propertyType = propertyType ? propertyType.split(',') : '';
+			let query = {
+				type: reType,
+				price: {
+					$gte: parseInt(!!minPrice ? minPrice : 0),
+					$lte: parseInt(!!maxPrice ? maxPrice : 999999999),
+				},
+				'about.rooms': {
+					$gte: parseInt(Boolean(minRooms) ? minRooms : 0),
+					$lte: parseInt(Boolean(maxRooms) ? maxRooms : 12),
+				},
+				'address.city': address[0] ? address[0] : { $regex: '.*' },
+				'address.street': address[1] ? address[1] : { $regex: '.*' },
+
+				'about.type': !!propertyType ? { $in: propertyType } : { $regex: '.*' },
+			};
+
+			flats = await RealEstate.find(query);
+		} else {
+			flats = await RealEstate.find({ type: reType });
+		}
+
+		flats = flats.map(flat => {
+			if (!flat.about.enteryDate) return flat;
+			let f = {
+				...flat.toObject(),
+				about: {
+					...flat.about.toObject(),
+					enteryDate: moment(flat.about.enteryDate).format('DD/MM/YYYY'),
+				},
+			};
+			return f;
+		});
+		res.send(flats);
+	} catch (e) {
+		console.log(e);
+		res.status(400).send(e);
+	}
+});
+module.exports=router
--- src/db/set-db.js
+++ src/db/set-db.js
@@ -1,231 +1,300 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/realEstate', {useUnifiedTopology: true,useNewUrlParser: true,useCreateIndex: true})
+mongoose.connect('mongodb://127.0.0.1:27017/realEstate', {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true,
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () =>
-    console.log("we're connected to db!")    
-);
+db.once('open', () => console.log("we're connected to db!"));
 // const housePicsLinks=[
- 
+
 // ]
-const {housePics,roomPics} =require('./pics')
+const { housePics, roomPics } = require('./pics');
 
-const RealEstate=require('./realEstateModel')
+const RealEstate = require('./realEstateModel');
 
-const realEstates=[
-    {
-        address:{city:'מבוא חורון',street:'יפה נוף',number:345},
-        price:2000000,
-        pictures:[housePics[0],roomPics[0]],
-        description:'דירה למגורים ולהשקעה ודאית! הדירה במתחם שאושר לעסקת פינוי-בינוי חתומה מול יזם, בליווי העירייה והמדינה בהליך מואץ לביצוע. דירת התמורה החדשה: 5 חדרים + מרפסת וחניה.',
-        propertyDetails:{
-            airConditioner:true,  
-            warhouse:true,
-            balcony:true,
-            accessibility:false,
-            renovated:false,
-            bars:false,
-            elevator:true,
-            parking:false,
-            accessibility:true,
-            kosher:true,
-            furniture:' נמכרת עם תכולה מלאה ( ריהוט, מכשירי חשמל, מזגנים בכל החדרים , גופי תאורה, ארונות קיר)'
-        },
-        about:{
-            type:"apartment",
-            shelter:true,
-            //bathroom:{...detail},
-            //storeroom:{...detail},
-            enteryDate:new Date('Wed Feb 15 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
-            //rented:,
-            rooms:9,
-            enteryFromStreet:true,
-            floor:5,
-            squaremeter:100
-            //meetingRoom:{...detail},
-          //  rentedTill:{type:Date,default:'not specified'},
-        }
-        
-    }, {
-        address:{city:"רחובות",street:'חזון איש',neighborhood:"רחובות הצעירה",number:4},
-        price:3000000,
-        pictures:[housePics[1],roomPics[1]],
-        description:'למכירה בהזדמנות בעיר רחובות!! ברחובות הצעירה בנין חדש משנת 2014 דירת גן 4 חדרים 100 מ"ר ממ"ד עם חלון חצר 100 מ"ר 2 חדרי רחצה מזגנים בכל הדירה 2 שירותים 2 מעליות 2 חניות לא עוקבות',
-        propertyDetails:{
-        airConditioner:true,  
-           warhouse:true,
-            balcony:true,
-            accessibility:true,
-            renovated:true,
-            bars:true,
-            elevator:true,
-            parking:false,
-            accessibility:true,
-            kosher:true,
-           // furniture:'dining table and chairs'
-        },
-        about:{
-            type:"apartment",
-            shelter:true,
-            //bathroom:{...detail},
-            //storeroom:{...detail},
-            enteryDate:new Date('Wed Jan 1 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
-            //rented:,
-            enteryFromStreet:false,
-            floor:12,
-            squaremeter:80,
-            rooms:3
-            //meetingRoom:{...detail},
-          //  rentedTill:{type:Date,default:'not specified'},
-        }
-        
-    },
-    {
-        address:{city:"להבים",street:'נחל ירוחם',number:9},
-        price:3500000,
-        pictures:[housePics[2],roomPics[2]],
-        description:'וילה חדשה פונקציונאלית ויוקרתית בשרונית מושקעת מאוד , מרוהטת ומעוצבת סוויטת הורים ענקית ומפוארת. מחסן חיצוני בנוי עם הכנה לקליניקה/משרד 3 שירותים ומקלחות. גינה ,מערכות מיגון וחימום מים על גז.',
-        propertyDetails:{
-            airConditioner:true,  
-            warhouse:true,
-            balcony:true,
-            accessibility:false,
-            renovated:false,
-            bars:false,
-            elevator:true,
-            parking:2,
-            accessibility:false,
-            kosher:false
-           // furniture:'dining table and chairs'
-        },
-        about:{
-            type:'private house',
-            shelter:true,
-            //bathroom:{...detail},
-            //storeroom:{...detail},
-            enteryDate:new Date('Wed Jan 15 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
-            //rented:,
-            enteryFromStreet:false,
-            floor:2,
-            squaremeter:105,
-            rooms:4
-            //meetingRoom:{...detail},
-          //  rentedTill:{type:Date,default:'not specified'},
-        }
-        
-    },{
-        address:{city:"נתניה",street:'אייר',neighborhood:"קרית השרון",number:4},
-        price:100000,
-        pictures:[housePics[3],roomPics[3]],
-        description:'פנטהאוז מדהים ומושקע , פונה לשדות של אבן יהודה ללא ביניינים קרובים משום צד. מרפסת 100 מטר מדהימה ומאובזרת',
-        propertyDetails:{
-            airConditioner:true,  
-            warhouse:true,
-            balcony:true,
-            accessibility:false,
-            renovated:true,
-            bars:true,
-            elevator:true,
-            parking:1,
-            accessibility:false,
-            kosher:true,
-            furniture:'ארונות קיר ושידות'
-        },
-        about:{
-            type:'penthouse',
-            shelter:true,
-            //bathroom:{...detail},
-            //storeroom:{...detail},
-            enteryDate:new Date('Wed Jan 15 2021 15:54:54 GMT+0200 (Israel Standard Time)'),
-            rented:true,
-            enteryFromStreet:true,
-            floor:9,
-            squaremeter:200,
-            rooms:2,
-            //meetingRoom:{...detail},
-          //  rentedTill:{type:Date,default:'not specified'},
-        }
-        
-    },
-    {
-        address:{city:"ירושלים",street:'נשר',neighborhood:"גילה",number:4},
-        price:3500000,
-        pictures:[housePics[4],roomPics[4]],
-        type:'forRent',
-         description:'3 כיווני אוויר הבית היחיד בנשר עם נוף של כל ירושלים בית אחרי שיפוץ כללי הכול כולל הכול אופציה לבניה ולהרחבה חימום מזגנים וגם יונקרס חשמל תלת פאזי תריסים חשמליים',
-         propertyDetails:{
-             airConditioner:true,  
-             warhouse:true,
-             balcony:true,
-             accessibility:true,
-             renovated:true,
-             bars:true,
-             elevator:true,
-             parking:1,
-             accessibility:true,
-             kosher:true,
-             furniture:'לפי הקיים במלאי וללא תוספת במחיר השכירות'
-         },
-         about:{
-             type:'duplex',
-             shelter:true,
-             //bathroom:{...detail},
-             //storeroom:{...detail},
-             enteryDate:new Date('Wed Jan 15 2021 15:54:54 GMT+0200 (Israel Standard Time)'),
-             rented:true,
-             enteryFromStreet:true,
-             floor:9,
-             squaremeter:200,
-             rooms:1,
-             //meetingRoom:{...detail},
-           //  rentedTill:{type:Date,default:'not specified'},
-         }
-         
-     },
-     {
-        address:{city:"דימונה",street:'שדרות ירושלים',neighborhood:"ממשית",number:19},
-        price:2700000,
-        pictures:[housePics[4],roomPics[4]],
-        type:'forRent',
-         description:'בית קרקע עם שתי חצרות, אחת מקדימה ואחת מאחור. הבית משופץ ומוכן לכניסה. שכנים טובים מאוד. כניסה באוגוסט. אפשר לקנות לפני ולהחזיק את השוכרים שנמצאים כרגע. נא להתקשר למספר טלפון השני במודעה - אורי.',
-         propertyDetails:{
-             airConditioner:true,  
-             warhouse:true,
-             balcony:true,
-             accessibility:true,
-             renovated:true,
-             bars:true,
-             elevator:true,
-             parking:1,
-              accessibility:true,
-            kosher:true,
-             furniture:'ארונות בכל הדירה, סלון, שטיחים, מיטת עמינח, חדר שינה ועוד. כולל דברי חשמל.'
-         },
-         about:{
-             type:'vacation house',
-             shelter:true,
-             //bathroom:{...detail},
-             //storeroom:{...detail},
-             enteryDate:new Date('Wed Jan 15 2021 15:54:54 GMT+0200 (Israel Standard Time)'),
-             rented:true,
-             enteryFromStreet:true,
-             floor:9,
-             squaremeter:200,
-             rooms:1,
-             //meetingRoom:{...detail},
-           //  rentedTill:{type:Date,default:'not specified'},
-         }
-         
-     },
-]
+const realEstates = [
+	{
+		address: { city: 'מבוא חורון', street: 'יפה נוף', number: 345 },
+		price: 2000000,
+		pictures: [housePics[0], roomPics[0]],
+		description:
+			'דירה למגורים ולהשקעה ודאית! הדירה במתחם שאושר לעסקת פינוי-בינוי חתומה מול יזם, בליווי העירייה והמדינה בהליך מואץ לביצוע. דירת התמורה החדשה: 5 חדרים + מרפסת וחניה.',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: false,
+			renovated: false,
+			bars: false,
+			elevator: true,
+			parking: false,
+			accessibility: true,
+			kosher: true,
+			furniture: ' נמכרת עם תכולה מלאה ( ריהוט, מכשירי חשמל, מזגנים בכל החדרים , גופי תאורה, ארונות קיר)',
+		},
+		about: {
+			type: 'apartment',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Feb 15 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
+			//rented:,
+			rooms: 9,
+			enteryFromStreet: true,
+			floor: 5,
+			squaremeter: 100,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+	{
+		address: { city: 'מבוא חורון', street: 'יפה נוף', number: 345 },
+		price:0 ,
+		pictures: [housePics[0], housePics[0]],
+		description:
+			'דירה למגורים ולהשקעה ודאית! הדירה במתחם שאושר לעסקת פינוי-בינוי חתומה מול יזם, בליווי העירייה והמדינה בהליך מואץ לביצוע. דירת התמורה החדשה: 5 חדרים + מרפסת וחניה.',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: false,
+			renovated: false,
+			bars: false,
+			elevator: true,
+			parking: false,
+			accessibility: true,
+			kosher: true,
+			furniture: ' נמכרת עם תכולה מלאה ( ריהוט, מכשירי חשמל, מזגנים בכל החדרים , גופי תאורה, ארונות קיר)',
+		},
+		about: {
+			type: 'apartment',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Feb 15 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
+			//rented:,
+			rooms: 9,
+			enteryFromStreet: true,
+			floor: 5,
+			squaremeter: 100,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},{
+		address: { city: 'רחובות', street: 'חזון איש', neighborhood: 'רחובות הצעירה', number: 4 },
+		price: 3000000,
+		//pictures: [housePics[1], roomPics[1]],
+		description:
+			'למכירה בהזדמנות בעיר רחובות!! ברחובות הצעירה בנין חדש משנת 2014 דירת גן 4 חדרים 100 מ"ר ממ"ד עם חלון חצר 100 מ"ר 2 חדרי רחצה מזגנים בכל הדירה 2 שירותים 2 מעליות 2 חניות לא עוקבות',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: true,
+			renovated: true,
+			bars: true,
+			elevator: true,
+			parking: false,
+			accessibility: true,
+			kosher: true,
+			// furniture:'dining table and chairs'
+		},
+		about: {
+			type: 'apartment',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Jan 1 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
+			//rented:,
+			enteryFromStreet: false,
+			floor: 12,
+			squaremeter: 80,
+			rooms: 3,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+	{
+		address: { city: 'רחובות', street: 'חזון איש', neighborhood: 'רחובות הצעירה', number: 4 },
+		price: 3000000,
+		pictures: [housePics[1], roomPics[1]],
+		description:
+			'למכירה בהזדמנות בעיר רחובות!! ברחובות הצעירה בנין חדש משנת 2014 דירת גן 4 חדרים 100 מ"ר ממ"ד עם חלון חצר 100 מ"ר 2 חדרי רחצה מזגנים בכל הדירה 2 שירותים 2 מעליות 2 חניות לא עוקבות',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: true,
+			renovated: true,
+			bars: true,
+			elevator: true,
+			parking: false,
+			accessibility: true,
+			kosher: true,
+			// furniture:'dining table and chairs'
+		},
+		about: {
+			type: 'apartment',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Jan 1 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
+			//rented:,
+			enteryFromStreet: false,
+			floor: 12,
+			squaremeter: 80,
+			rooms: 3,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+	{
+		address: { city: 'להבים', street: 'נחל ירוחם', number: 9 },
+		price: 3500000,
+		pictures: [housePics[2], roomPics[2]],
+		description:
+			'וילה חדשה פונקציונאלית ויוקרתית בשרונית מושקעת מאוד , מרוהטת ומעוצבת סוויטת הורים ענקית ומפוארת. מחסן חיצוני בנוי עם הכנה לקליניקה/משרד 3 שירותים ומקלחות. גינה ,מערכות מיגון וחימום מים על גז.',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: false,
+			renovated: false,
+			bars: false,
+			elevator: true,
+			parking: 2,
+			accessibility: false,
+			kosher: false,
+			// furniture:'dining table and chairs'
+		},
+		about: {
+			type: 'private house',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Jan 15 2020 15:54:54 GMT+0200 (Israel Standard Time)'),
+			//rented:,
+			enteryFromStreet: false,
+			floor: 2,
+			squaremeter: 105,
+			rooms: 4,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+	{
+		address: { city: 'נתניה', street: 'אייר', neighborhood: 'קרית השרון', number: 4 },
+		price: 100000,
+		pictures: [housePics[3], roomPics[3]],
+		description:
+			'פנטהאוז מדהים ומושקע , פונה לשדות של אבן יהודה ללא ביניינים קרובים משום צד. מרפסת 100 מטר מדהימה ומאובזרת',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: false,
+			renovated: true,
+			bars: true,
+			elevator: true,
+			parking: 1,
+			accessibility: false,
+			kosher: true,
+			furniture: 'ארונות קיר ושידות',
+		},
+		about: {
+			type: 'penthouse',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Jan 15 2021 15:54:54 GMT+0200 (Israel Standard Time)'),
+			rented: true,
+			enteryFromStreet: true,
+			floor: 9,
+			squaremeter: 200,
+			rooms: 2,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+	{
+		address: { city: 'ירושלים', street: 'נשר', neighborhood: 'גילה', number: 4 },
+		price: 3500000,
+		pictures: [housePics[4], roomPics[4]],
+		type: 'forRent',
+		description:
+			'3 כיווני אוויר הבית היחיד בנשר עם נוף של כל ירושלים בית אחרי שיפוץ כללי הכול כולל הכול אופציה לבניה ולהרחבה חימום מזגנים וגם יונקרס חשמל תלת פאזי תריסים חשמליים',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: true,
+			renovated: true,
+			bars: true,
+			elevator: true,
+			parking: 1,
+			accessibility: true,
+			kosher: true,
+			furniture: 'לפי הקיים במלאי וללא תוספת במחיר השכירות',
+		},
+		about: {
+			type: 'duplex',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Jan 15 2021 15:54:54 GMT+0200 (Israel Standard Time)'),
+			rented: true,
+			enteryFromStreet: true,
+			floor: 9,
+			squaremeter: 200,
+			rooms: 1,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+	{
+		address: { city: 'דימונה', street: 'שדרות ירושלים', neighborhood: 'ממשית', number: 19 },
+		price: 2700000,
+		pictures: [housePics[4], roomPics[4]],
+		type: 'forRent',
+		description:
+			'בית קרקע עם שתי חצרות, אחת מקדימה ואחת מאחור. הבית משופץ ומוכן לכניסה. שכנים טובים מאוד. כניסה באוגוסט. אפשר לקנות לפני ולהחזיק את השוכרים שנמצאים כרגע. נא להתקשר למספר טלפון השני במודעה - אורי.',
+		propertyDetails: {
+			airConditioner: true,
+			warhouse: true,
+			balcony: true,
+			accessibility: true,
+			renovated: true,
+			bars: true,
+			elevator: true,
+			parking: 1,
+			accessibility: true,
+			kosher: true,
+			furniture: 'ארונות בכל הדירה, סלון, שטיחים, מיטת עמינח, חדר שינה ועוד. כולל דברי חשמל.',
+		},
+		about: {
+			type: 'vacation house',
+			shelter: true,
+			//bathroom:{...detail},
+			//storeroom:{...detail},
+			enteryDate: new Date('Wed Jan 15 2021 15:54:54 GMT+0200 (Israel Standard Time)'),
+			rented: true,
+			enteryFromStreet: true,
+			floor: 9,
+			squaremeter: 200,
+			rooms: 1,
+			//meetingRoom:{...detail},
+			//  rentedTill:{type:Date,default:'not specified'},
+		},
+	},
+];
 
 // state.forEach(car => {
 // new Car(car).save(e=>
 //     console.log('car: '+car.plateNumber,e)
-//     ) 
+//     )
 // });
 //RealEstate.remove({}, ()=>console.log('remove'));
-RealEstate.insertMany(realEstates,e=>
-    console.log(e)
-)
+RealEstate.insertMany(realEstates, e => console.log(e));
